b07aca8000bc344de93ade0e3f6151bac3c64a40
gmadro04 <gmadro0405@gmail.com>
2025-09-02 20:22:49 -0500

    Fixing errors to compile and find packages to compile ARES

--- build/CMakeFiles/loop_function.dir/DependInfo.cmake
+++ build/CMakeFiles/loop_function.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/gmadro/swarm_robotics/SWARM_GENERATOR/src/loop_function.cpp" "/home/gmadro/swarm_robotics/SWARM_GENERATOR/build/CMakeFiles/loop_function.dir/loop_function.cpp.o"
+  "/home/gmadro/swarm_robotics/ARES/src/loop_function.cpp" "/home/gmadro/swarm_robotics/ARES/build/CMakeFiles/loop_function.dir/loop_function.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
